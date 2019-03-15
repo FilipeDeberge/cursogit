746eac8c7916e5668b9a90800e7e879ef8a2e0f1
Filipe Deberge <filipedeberge@gmail.com>
2019-03-15 09:57:13 -0300

    Projeto iniciado indicando os tipos de dados em c#.

---
+++ EntradadeDados/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace EntradadeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma frase:");
+            string frase = Console.ReadLine();
+            Console.WriteLine("Digite uma cor:");
+            string x = Console.ReadLine();
+            Console.WriteLine("Digite uma cor:");
+            string y = Console.ReadLine();
+            Console.WriteLine("Digite uma cor:");
+            string z = Console.ReadLine();
+            Console.WriteLine("Digite três cores separadas por espaço:");
+            
+
+            string[] v = Console.ReadLine().Split(' ');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2]; 
+
+            
+
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            
+        }
+    }
+}
